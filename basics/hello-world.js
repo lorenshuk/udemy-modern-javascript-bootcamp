9a3a78143f97ae04f2107e751797a18d212c2ec7
loren inada <lordawg375@sbcglobal.net>
2020-01-24 14:45:21 -0800

    Done 1/24/20 2:44 PM

---
+++ basics/hello-world.js
@@ -0,0 +1,29 @@
+// console.log('Hello World!')
+
+// let name = 'Hello JackAss!'
+// console.log(name)
+
+// let firstName = 'Jack'
+// let lastName = 'Ass'
+// let fullName = firstName + (lastName != '' ? ' ' + lastName: '')
+// console.log(fullName)
+
+// lastName = ''
+// fullName = firstName + (lastName != '' ? ' ' + lastName : '')
+// console.log(fullName)
+
+// let city = ' Boise '
+// console.log(location)
+// let location = city.trim() + (city.trim()!='' ? ', ' + state.trim() : state.trim())
+// let state = '  ID  '
+
+// Lesson 13 - Temperature Converter Challenge
+let farenheit = 69
+let kelvinConst = 273.15
+let celsiusCalc = (farenheit - 32) * (5/9)
+let kelvinCalc = celsiusCalc + kelvinConst 
+
+console.log("Farenheit:" + farenheit)
+console.log("Celsius:" + celsiusCalc)
+console.log("Kelvin:" + kelvinCalc)
+
