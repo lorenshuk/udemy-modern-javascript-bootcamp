6c62984d34ac706a5d918bf1da41ff34fa477654
loren inada <lordawg375@sbcglobal.net>
2020-02-05 19:13:01 -0800

    Save meager changes 2/5/20 7:12 PM

--- basics/hello-world.js
+++ basics/hello-world.js
@@ -18,7 +18,7 @@
 // let state = '  ID  '
 
 // Lesson 13 - Temperature Converter Challenge
-let farenheit = 69
+/* let farenheit = 69
 let kelvinConst = 273.15
 let celsiusCalc = (farenheit - 32) * (5/9)
 let kelvinCalc = celsiusCalc + kelvinConst 
@@ -26,4 +26,11 @@ let kelvinCalc = celsiusCalc + kelvinConst
 console.log("Farenheit:" + farenheit)
 console.log("Celsius:" + celsiusCalc)
 console.log("Kelvin:" + kelvinCalc)
+//  */
+let age = 97
+let child = age <= 7
+let senior = age >= 65
 
+console.log('AGE: ' + age)
+console.log(child ? "Is Child" : 'Not a Child')
+console.log(senior ? 'You are OLD! Ha ha ha ha!' : "Good job you haven't aged!")
