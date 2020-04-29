05e6c7c390234b66708a35f0c52732ce9c7ff2da
loren inada <lordawg375@sbcglobal.net>
2020-04-29 16:06:42 -0700

    Done Lesson 75. 4/29/20 4:06 PM

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -13,7 +13,7 @@ document.querySelector('#create-note').addEventListener('click', function (e) {
    
    notes.push({
       id:noteID,
-      title: `(Untitled Note)`,
+      title: ``,
       body: ''
    })
 
@@ -37,3 +37,49 @@ window.addEventListener('storage', e => {
       renderNotes(notes, filters)
    }
 })
+
+
+/**********************************
+      Lesson 74: Date Manipulation 
+***********************************/
+// let now = new Date()
+/* console.log(`Today's Date: ${now.toString()}`)
+let now = new Date()
+console.log(`Year: ${now.getFullYear()}`)
+console.log(`Month: ${now.getMonth()}`)
+console.log(`Day: ${now.getDate()}`)
+
+console.log(`Hour: ${now.getHours()}`)
+console.log(`Minute: ${now.getMinutes()}`)
+console.log(`Second: ${now.getSeconds()}`)
+ console.log(`# Unix Epoch: ${now.getTime()}`) 
+
+let newDate = new Date(`January 1 1970 12:00:00`)
+let timeStamp = newDate.getTime()
+
+console.log(`newDate: ${newDate}`)
+console.log(`timeStamp: ${timeStamp}`)
+console.log(`Year: ${newDate.getFullYear()}`)
+ 
+
+ // Challenge: Sorting 2 Dates
+const date1 = new Date(`February 1 1969 12:31:25`)
+const date2 = new Date(`February 1 2019 00:00:01`)
+
+const timeStamp1 = date1.getTime()
+const timeStamp2 = date2.getTime()
+
+console.log(`First Date: ${date1}`)
+console.log(`Second Date: ${date2}`)
+
+if (timeStamp1 < timeStamp2) {
+   console.log(`Sorted Date: ${date1}`)
+} else {
+	console.log(`Sorted Date: ${ date2 }`)
+}
+*/
+
+/* Lesson 75: MOMENT Library */
+//let momDate = moment([1967, 0, 25])
+let momDate = moment().year(1925).month(11).date(25)
+console.log(momDate.format('MMM D, YYYY'))
