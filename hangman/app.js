5e0bc5a34c92a6e3b8a54c513882976d1d3945f0
loren inada <lordawg375@sbcglobal.net>
2020-08-21 14:58:39 -0700

    Save the hangman game before class reconstruction. 8/21/20 2:58 PM

--- hangman/app.js
+++ hangman/app.js
@@ -1,10 +1,99 @@
-const product = {
-    name: 'Influence'
+Hangman.prototype.getPuzzle = function () {
+    let guessShow = ''
+    let winner = true
+    const wordArray = this.guessWord.toLowerCase().split('')
+
+    wordArray.forEach(letter => {
+        if (letter === ' ') {
+            guessShow += ' '
+        } else if (this.guessedLetters.includes(letter)) {
+            guessShow += letter;
+        } else {
+            guessShow += '*'
+            winner = false
+        }
+    });
+
+    return guessShow
 }
 
-// hasOwnProperty() - check if the Class a property
-console.log(product.hasOwnProperty('name'))
-// Override the existing method "hasOwnProperty"
-Object.prototype.hasOwnProperty = () => 'This is an override of hasOwnProperty()'
-    // Display the new method
-console.log(product.hasOwnProperty('name'))
+Hangman.prototype.makeGuess = function (guess) {
+    guess = guess.toLowerCase()
+    const isUnique = !this.guessedLetters.includes(guess)
+    const isBadGuess = !this.guessWord.toLowerCase().split('').includes(guess)
+
+    if (isUnique) {
+        this.guessedLetters.push(guess)
+        this.guessedLetters.sort()
+    }
+
+    if (isUnique && isBadGuess)
+        this.guessLimit--
+
+    this.getStatus()
+}
+
+Hangman.prototype.getStatus = function () {
+    let winner = true
+    const wordArray = this.guessWord.toLowerCase().split('')
+
+    if (this.guessLimit <= 0)
+        this.status = 'Failed'
+    else {
+        wordArray.forEach(letter => {
+            if (letter != ' ' && !this.guessedLetters.includes(letter)) {
+                winner = false
+            }
+        });
+    
+        console.log(`getStatus: ${winner}`)
+
+        if (winner)
+            this.status = 'Finished'
+        else    
+            this.status = 'Playing'
+    }
+}
+
+    /* Lesson 98 */
+Hangman.prototype.statusMessage = function (status) {
+    let returnString = ''
+
+    console.log(`statusMessage: ${status}`)
+
+    switch (status) {
+        case 'Failed':
+            returnString = `Nice try! The word was ${this.guessWord}.`;
+            break;
+        case 'Playing':
+            returnString = `Guesses Left: ${this.guessLimit}`;
+            break;
+        default:
+            returnString = `Great work! You guessed the word!`
+            break;
+    }
+
+    return returnString
+}
+
+/*****   Test Games   ******/
+var game1 = new Hangman('Totally Awesome', 5)
+const puzzleEl = document.querySelector('#puzzle')
+const guessesEl = document.querySelector('#guesses')
+const statusEl = document.querySelector('#status')
+
+statusEl.textContent = game1.status
+puzzleEl.textContent = game1.getPuzzle()
+guessesEl.textContent = `Letters used: ${game1.guessedLetters}`
+
+window.addEventListener('keypress', function (e) {
+    if (game1.status === 'Playing') {
+        // use "fromCharCode()" method
+        const guess = String.fromCharCode(e.charCode)
+        game1.makeGuess(guess)
+        
+        statusEl.textContent = game1.statusMessage(game1.status)
+        puzzleEl.textContent = game1.getPuzzle();
+        guessesEl.textContent = `Letters used: ${game1.guessedLetters}`
+    }
+})
--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,62 +1,6 @@
 const Hangman = function (guessWord, guessLimit) {
-    this.guessWord = guessWord.toLowerCase()
+    this.guessWord = guessWord
     this.guessLimit = guessLimit
-    this.wordArray = guessWord.split('')
     this.guessedLetters = []
+    this.status = 'Playing'
 }
-
-Hangman.prototype.Puzzle = function () {
-    let guessString = '[No guesses] '
-    let guessShow = ''
-    let rightGuess = false
-
-    if (this.guessedLetters.length > 0) {
-        guessString = 'Guesses: '
-        let i = 0
-
-        this.guessedLetters.forEach(letter => {
-            guessString = guessString + (i>0 ? ', ' : '') + `"${letter}"`
-            i++
-        })
-    }
-
-    this.wordArray.forEach(letter => {
-        if (letter == ' ') {
-            guessShow += ' '
-        } else if (this.guessedLetters.includes(letter.toLowerCase())) {
-            guessShow += letter;
-        } else {
-            guessShow += '*'
-        }
-    });
-    
-    return `${guessString} -> ${guessShow} : Guesses Left ${this.guessLimit}`
-}
-
-Hangman.prototype.makeGuess = function (guess) {
-    guess = guess.toLowerCase()
-    const isUnique = !this.guessedLetters.includes(guess)
-    const isBadGuess = !this.guessWord.split('').includes(guess)
-
-    if (isUnique)
-        this.guessedLetters.push(guess)
-
-    if (isUnique && isBadGuess)
-        this.guessLimit--
-}
-
-/*
-    Test Games
-*/
-var hangman1 = new Hangman('Totally Awesome', 5)
-
-// GAME 2
-var hangman2 = new Hangman('Tennessee', 5)
-
-
-window.addEventListener('keypress', function (e) {
-    // use "fromCharCode()" method
-    const guess = String.fromCharCode(e.charCode)
-    hangman1.makeGuess(guess)
-    console.log(hangman1.Puzzle())
-})
