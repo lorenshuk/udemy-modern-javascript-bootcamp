980fe94bf220d2e2a502cb00805ddd09be18f225
loren inada <lordawg375@sbcglobal.net>
2021-02-07 21:45:40 -0800

    Done. 2/7/21 9:45 PM

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -1,5 +1,10 @@
 const { Module } = require("webpack");
+const path = require('path')
 
 module.exports = {
-    
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public/scripts'),
+        filename: 'bundle.js'
+    }
 }
