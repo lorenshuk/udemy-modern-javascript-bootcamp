db12f5c48e13018bcde0934ae49ab9a6a9c5af78
loren inada <lordawg375@sbcglobal.net>
2021-02-14 18:06:18 -0800

    Done 2/14/21 6:06 PM

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,11 +1,8 @@
-const name = 'Mike'
-console.log(name)
+// Import utilities.js script
+import   add, { multi } from './utilities'
+import screamingBitch from './scream'
 
-class Hangman {
-    myMethod() {
-        return 'Testing'
-    }
-}
-
-const hangman = new Hangman()
-console.log(hangman.myMethod())
+// console.log('index.js')
+console.log(add(32,38))
+console.log(multi(32, 38))
+console.log(screamingBitch('You are me. who is THINKER?'))
---
+++ boilerplate/src/scream.js
@@ -0,0 +1,6 @@
+// 2/14/21 Lesson 135: JS Modules Challenge
+function scream(inString) {
+    return inString.trim().toUpperCase() + '!'
+}
+
+export default scream
