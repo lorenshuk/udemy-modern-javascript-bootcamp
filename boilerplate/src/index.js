380a8aae23a2c214053730e0db29353dd94fc915
loren inada <lordawg375@sbcglobal.net>
2021-02-15 11:45:21 -0800

    Create new branch hangman20

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -5,4 +5,4 @@ import screamingBitch from './scream'
 // console.log('index.js')
 console.log(add(32,38))
 console.log(multi(32, 38))
-console.log(screamingBitch('You are me. who is THINKER?'))
+console.log(screamingBitch('You are me. who is THINKER? No me'))
