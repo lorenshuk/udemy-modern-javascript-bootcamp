05e7776c4e1514a71d4acc03c9730e3eba0282c1
loren inada <lordawg375@sbcglobal.net>
2021-02-16 16:36:16 -0800

    Done 2/16/21 4:36 PM

--- notes-app20/src/index.js
+++ notes-app20/src/index.js
@@ -1,1 +1,30 @@
-console.log('Index.js')
+import { getNotes, createNote, removeNote, sortNotes, updateNote  } from './notes'
+import { getFilters, setFilters } from './filters'
+
+// Test code for the construction 2/16/21
+// console.log(getNotes())
+// createNote()
+// createNote()
+// createNote()
+// createNote()
+// console.log(getNotes())
+
+// Remove Note
+// console.log('Remove Note Test "b572a912-1b90-49e3-9673-073483463a31"')
+// removeNote("b572a912-1b90-49e3-9673-073483463a31")
+// console.log(getNotes())
+
+// // Update Note
+// console.log('Update Note Test')
+// updateNote("6ba8d94f-967b-4891-ad17-7cfa4cb92218", {
+//     title: 'The Best Title Ever!',
+//     body: 'This is a great body. SY!'
+// })
+
+/*** Test Filters Module ***/
+console.log(getFilters())
+setFilters({
+    searchText: 'office',
+    sortBy: 'byCreated'
+})
+console.log(getFilters())
--- notes-app20/src/notes.js
+++ notes-app20/src/notes.js
@@ -1,3 +1,8 @@
+import { v4 as uuidv4 } from 'uuid'
+import moment from 'moment'
+
+let notes = []
+
 // Retrieve the notes object from localStorage
 function loadNotes() {
     const notesJSON = localStorage.getItem('notes')
@@ -11,24 +16,97 @@ function loadNotes() {
 
 }
 
-// Expose Notes to other files
-const getNotes = () => notes
-
-notes = loadNotes()
-
 // Save notes
-const saveNotes = notes => {
+const saveNotes = () => {
     localStorage.setItem('notes', JSON.stringify(notes))
 }
+
+// Expose Notes to other files
+let getNotes = () => notes
+
+// Create a note object/element for the array
+const createNote = () => {
+    const noteID = uuidv4()
+    const timeStamp = moment().valueOf()
+    
+    notes.push({
+        id: noteID,
+        title: ``,
+        body: '',
+        createdAt: timeStamp,
+        updatedAt: timeStamp
+    })    
+    saveNotes()
+}    
 
 const removeNote = id => {
     const noteIndex = notes.findIndex(note => {
         return note.id === id
     })
-
+    
     if (noteIndex > -1) {
         notes.splice(noteIndex, 1)
+        saveNotes()
     }
 }
 
-export { getNotes }
+const sortNotes = (sortBy) => {
+    if (sortBy === 'byEdited') {
+        return notes.sort((a, b) => {
+            if (a.updatedAt > b.updatedAt) {
+                return -1
+            } else if (a.updatedAt < b.updatedAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'byCreated') {
+        return notes.sort((a, b) => {
+            if (a.createdAt > b.createdAt) {
+                return -1
+            } else if (a.createdAt < b.createdAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy = 'alphabetical') {
+        return notes.sort((a, b) => {
+            if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                return -1
+            } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else {
+        return notes
+    }
+}
+
+const updateNote = (id, updates) => {
+    // 1. Verify the ID paramater - find the correct note element
+    const note = notes.find((note) => note.id == id)
+    const timeStamp = moment().valueOf()
+
+    // 2. Make sure the note is found
+    if (!note) {
+        return
+    }
+
+    if (typeof updates.title == 'string' && updates.title != note.title) {
+        note.title = updates.title
+        note.updateAt = timeStamp
+    }
+    if (typeof updates.body == 'string' && updates.body != note.body) {
+        note.body = updates.body
+        note.updateAt = timeStamp
+    }
+    saveNotes()
+}
+
+notes = loadNotes()
+
+export { getNotes, createNote, removeNote, sortNotes, updateNote }
